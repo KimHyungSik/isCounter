394861a4d64f916e072e65ff90b75f304cd5679f
KimHyungSik <colrog968@gmail.com>
2023-07-23 21:48:09 +0900

    sharedpreferences 타입 명세

--- lib/database/prefs/sharedpreferences.dart
+++ lib/database/prefs/sharedpreferences.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:is_counter/database/prefs_key.dart';
+import 'package:is_counter/database/prefs/prefs_key.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Prefs {
@@ -12,19 +12,19 @@ class Prefs {
     return _sharedpreferences;
   }
 
-  read(PREFS_KEY key) async {
+  Future<String?> read(PREFS_KEY key) async {
     final prefs = await SharedPreferences.getInstance();
     final value = prefs.getString(key.name);
     if (value == null) return null;
     return value;
   }
 
-  save(PREFS_KEY key, dynamic value) async {
+  Future save(PREFS_KEY key, dynamic value) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString(key.name, jsonEncode(value));
   }
 
-  remove(PREFS_KEY key) async {
+  Future remove(PREFS_KEY key) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.remove(key.name);
   }
