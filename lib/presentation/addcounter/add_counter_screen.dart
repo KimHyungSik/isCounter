4bf86c796a7a09cabb25bcc601ef1d54d6ac3292
KimHyungSik <colrog968@gmail.com>
2023-07-31 22:20:35 +0900

    ColorPicker file

--- lib/presentation/addcounter/add_counter_screen.dart
+++ lib/presentation/addcounter/add_counter_screen.dart
@@ -4,6 +4,8 @@ import 'package:is_counter/presentation/addcounter/add_counter_viewmodel.dart';
 import 'package:is_counter/presentation/appbar/app_bar.dart';
 import 'package:provider/provider.dart';
 
+import '../common/color_picker.dart';
+
 class AddCounterScreen extends StatelessWidget {
   AddCounterScreen({super.key});
 
@@ -87,54 +89,6 @@ class AddCounterScreen extends StatelessWidget {
           },
         );
       },
-    );
-  }
-}
-
-class ColorPicker extends StatelessWidget {
-  ColorPicker({
-    super.key,
-    this.selected = 0,
-    required this.click,
-  });
-
-  final int selected;
-  final void Function(int) click;
-
-  final List<Color> colors = [
-    Colors.red,
-    Colors.orange,
-    Colors.yellow,
-    Colors.green,
-    Colors.blue,
-    Colors.indigo,
-    Colors.purple,
-    Colors.pink,
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-      child: Row(
-        children: colors.asMap().entries.map((entry) {
-          return Flexible(
-            flex: 1,
-            child: GestureDetector(
-              onTap: () {
-                click(entry.key);
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  border: selected == entry.key ? Border.all() : null,
-                  color: entry.value,
-                ),
-                height: 40,
-              ),
-            ),
-          );
-        }).toList(),
-      ),
     );
   }
 }
---
+++ lib/presentation/common/color_picker.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ColorPicker extends StatelessWidget {
+  ColorPicker({
+    super.key,
+    this.selected = 0,
+    required this.click,
+  });
+
+  final int selected;
+  final void Function(int) click;
+
+  final List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+    Colors.pink,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+      child: Row(
+        children: colors.asMap().entries.map((entry) {
+          return Flexible(
+            flex: 1,
+            child: GestureDetector(
+              onTap: () {
+                click(entry.key);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  border: selected == entry.key ? Border.all() : null,
+                  color: entry.value,
+                ),
+                height: 40,
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
