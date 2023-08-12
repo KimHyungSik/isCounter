9513569d3131221bb5a5a539d227285f6bf95251
KimHyungSik <colrog968@gmail.com>
2023-08-12 22:27:44 +0900

    리스트 뷰 만들기

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:is_counter/database/model/counter/counter.dart';
 import 'package:is_counter/presentation/appbar/app_bar.dart';
 import 'package:provider/provider.dart';
 import 'package:is_counter/route/route.dart' as route;
 
+import '../common/color_picker.dart';
 import 'main_viewmodel.dart';
 
 class MainScreen extends StatelessWidget {
@@ -28,8 +30,59 @@ class MainScreen extends StatelessWidget {
         itemCount: viewModel.counterList.list.length,
         itemBuilder: ((context, index) {
           final counter = viewModel.counterList.list[index];
-          return Text(counter.title);
+          return NumberListItem(counter: counter);
         }),
+      ),
+    );
+  }
+}
+
+class NumberListItem extends StatelessWidget {
+  final Counter counter;
+
+  const NumberListItem({super.key, required this.counter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        color: counterColors[counter.color],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                counter.title,
+                style: const TextStyle(fontSize: 18),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      // 버튼을 누를 때 동작
+                    },
+                  ),
+                  const Spacer(),
+                  Text(
+                    counter.value.toString(),
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const Spacer(), // 나머지 공간 차지
+                  IconButton(
+                    icon: const Icon(Icons.remove),
+                    onPressed: () {
+                      // 버튼을 누를 때 동작
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/presentation/main/main_viewmodel.dart
+++ lib/presentation/main/main_viewmodel.dart
@@ -9,13 +9,14 @@ import '../../database/prefs/prefs_key.dart';
 import '../../database/prefs/sharedpreferences.dart';
 
 class MainViewModel extends ChangeNotifier {
+  CounterList _counterList = CounterList();
+  CounterList get counterList => _counterList;
+  final CounterController _counterController = CounterController();
+
   MainViewModel() {
     getCounterList();
     print("create MainViewModel");
   }
-  CounterList _counterList = CounterList();
-  CounterList get counterList => _counterList;
-  final CounterController _counterController = CounterController();
 
   Future getCounterList() async {
     final counterList = await _counterController.getCounterList();
