aaa80b1eaec9c80dc1a7cbf3c9678c02deacd4c7
KimHyungSik <colrog968@gmail.com>
2023-08-20 20:12:01 +0900

    CounterListItem 수정

--- lib/presentation/widgets/counter_list_item.dart
+++ lib/presentation/widgets/counter_list_item.dart
@@ -22,43 +22,43 @@ class CounterListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        color: counterColors[counter.color],
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Text(
-                counter.title,
-                style: const TextStyle(fontSize: 18),
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.remove),
-                    onPressed: () => decrementValue(),
-                  ),
-                  const Spacer(),
-                  GestureDetector(
-                    onTap: () => navigatorCounterScreen(counter),
-                    child: Consumer<MainViewModel>(
+      title: GestureDetector(
+        onTap: () => navigatorCounterScreen(counter),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          color: counterColors[counter.color],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Text(
+                  counter.title,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.remove),
+                      onPressed: () => decrementValue(),
+                    ),
+                    const Spacer(),
+                    Consumer<MainViewModel>(
                       builder: (context, viewModel, _) => Text(
                         viewModel.getCounter(counter.id).value.toString(),
                         style: const TextStyle(fontSize: 18),
                       ),
                     ),
-                  ),
-                  const Spacer(),
-                  IconButton(
-                    icon: const Icon(Icons.add),
-                    onPressed: () => incrementValue(),
-                  ),
-                ],
-              ),
-            ],
+                    const Spacer(),
+                    IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () => incrementValue(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
