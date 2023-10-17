76691e027d247bf472813f962173d248b549d948
KimHyungSik <colrog968@gmail.com>
2023-10-17 22:23:31 +0900

    icon 추가

--- lib/common/counter_icon.dart
+++ lib/common/counter_icon.dart
@@ -5,7 +5,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 enum CounterIcon {
   add("${iconUrl}icon_24_add.svg"),
-  delete("${iconUrl}icon_24_delete.svg");
+  delete("${iconUrl}icon_24_delete.svg"),
+  plus("${iconUrl}icon_26_plus.svg"),
+  minus("${iconUrl}icon_26_minus.svg");
 
   const CounterIcon(this.url);
   final String url;
