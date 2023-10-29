93c1a44b6c92d63217aa9fad3b216250092ea8ec
KimHyungSik <colrog968@gmail.com>
2023-10-29 21:34:19 +0900

    ads helper

---
+++ lib/common/ads/ad_helper.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AdHelper {
+  static String get bannerAdUnitId {
+    final isDebug = kDebugMode;
+    try {
+      if (Platform.isAndroid) {
+        return isDebug
+            ? dotenv.env["Dev_google_ad_android_banner_key"]!
+            : dotenv.env["Google_ad_android_banner_key"]!;
+      } else if (Platform.isIOS) {
+        return isDebug
+            ? dotenv.env["Dev_google_ad_ios_banner_key"]!
+            : dotenv.env["Goolge_ad_ios_banner_key"]!;
+      } else {
+        throw UnsupportedError('Unsupported platform');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
